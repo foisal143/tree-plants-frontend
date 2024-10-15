04e37d35927f7cd419ba5b9618b2e3d7282587e1
Md Foisal Islam <islammdfoisal54@gmail.com>
2024-10-15 10:09:35 +0600

    add bg-image in subscribed section

--- src/pages/Home/SubscribedSection/SubscribedSection.tsx
+++ src/pages/Home/SubscribedSection/SubscribedSection.tsx
@@ -3,9 +3,9 @@ import HeadingText from '../../../components/HeadingText';
 
 const SubscribedSection = () => {
   return (
-    <div className="mb-[116px]">
+    <div className="mb-[116px] bg-[url(https://static.vecteezy.com/system/resources/thumbnails/049/127/603/small/a-small-tree-is-growing-in-a-greenhouse-photo.jpg)] py-12 bg-fixed w-full bg-cover bg-center">
       <Container>
-        <HeadingText style="text-center" heading="Subscribed Now" />
+        <HeadingText style="text-center text-white" heading="Subscribed Now" />
         <div className="mt-12 text-center w-full">
           <div className="join lg:w-1/2 mx-auto">
             <input
