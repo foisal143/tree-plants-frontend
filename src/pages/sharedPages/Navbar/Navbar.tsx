089e1ded041e85ced5ea48194b7fa7765d61bf4c
Md Foisal Islam <islammdfoisal54@gmail.com>
2024-10-17 10:29:47 +0600

    some chagnes in navbar

--- src/pages/sharedPages/Navbar/Navbar.tsx
+++ src/pages/sharedPages/Navbar/Navbar.tsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   }, []);
   return (
     <nav
-      className={`fixed w-full  top-0 left-0 z-auto transition-all duration-300 ${
+      className={`fixed w-full  top-0 left-0 z-50 transition-all duration-300 ${
         scrolled
           ? 'bg-white shadow-lg'
           : `bg-transparent  ${paths.includes(pathname) && 'text-white'}`
