fcc2a106a3222d7823ec4ebfc20905e6a5785f64
Md Foisal Islam <islammdfoisal54@gmail.com>
2024-10-22 12:36:25 +0600

    navbar bug fixed

--- src/pages/sharedPages/Navbar/Navbar.tsx
+++ src/pages/sharedPages/Navbar/Navbar.tsx
@@ -62,7 +62,7 @@ const Navbar = () => {
               !toggleMenu
                 ? 'lg:left-0 -left-[1250px]'
                 : 'left-0 lg:-left-[1250px]'
-            } lg:relative lg:top-0 absolute top-20 left-0 transition-all duration-300 rounded-md lg:bg-transparent p-5 lg:p-0 lg:w-fit lg:text-inherit w-full text-center text-black`}
+            } lg:relative lg:top-0 absolute top-20 transition-all duration-300 rounded-md lg:bg-transparent p-5 lg:p-0 lg:w-fit lg:text-inherit w-full text-center text-black`}
           >
             <ul className="lg:flex space-y-5 lg:space-y-0  items-center navlink gap-5">
               <li>
